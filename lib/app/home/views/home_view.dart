5d0c0a5340e0f6104d299c04b4013628da167a25
Julio <jucesargois@gmail.com>
2022-02-23 21:16:37 -0300

    Inserido gestureDetector em um dos widgets para que fosse possivel inserir o navigator.

--- lib/app/home/views/home_view.dart
+++ lib/app/home/views/home_view.dart
@@ -79,91 +79,96 @@ class HomeView extends StatelessWidget {
                 ),
                 child: Column(
                   children: [
-                    Container(
-                      width: 400,
-                      height: 117,
-                      decoration: BoxDecoration(
-                        image: const DecorationImage(
-                            image: AssetImage(
-                              'assets/images/image_card_home_star.png',
-                            ),
-                            fit: BoxFit.fill),
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.only(
-                              top: 10.0,
-                              left: 13.0,
-                              bottom: 10,
-                            ),
-                            child: Column(
-                              children: const [
-                                Text(
-                                  'My Location',
-                                  style: TextStyle(
-                                      fontSize: 25,
+                    GestureDetector(
+                      onTap: () {
+                         Navigator.pushReplacementNamed(context, "/weather_location");
+                      },
+                      child: Container(
+                        width: 400,
+                        height: 117,
+                        decoration: BoxDecoration(
+                          image: const DecorationImage(
+                              image: AssetImage(
+                                'assets/images/image_card_home_star.png',
+                              ),
+                              fit: BoxFit.fill),
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                top: 10.0,
+                                left: 13.0,
+                                bottom: 10,
+                              ),
+                              child: Column(
+                                children: const [
+                                  Text(
+                                    'My Location',
+                                    style: TextStyle(
+                                        fontSize: 25,
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  Text(
+                                    'Seongnam-si',
+                                    style: TextStyle(
+                                      fontSize: 16,
                                       color: Colors.white,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                                Text(
-                                  'Seongnam-si',
-                                  style: TextStyle(
-                                    fontSize: 16,
-                                    color: Colors.white,
+                                    ),
                                   ),
-                                ),
-                                SizedBox(
-                                  height: 28,
-                                ),
-                                Text(
-                                  'Partly Cloudy',
-                                  style: TextStyle(
-                                    fontSize: 16,
-                                    color: Colors.white,
+                                  SizedBox(
+                                    height: 28,
                                   ),
-                                ),
-                              ],
+                                  Text(
+                                    'Partly Cloudy',
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ],
+                              ),
                             ),
-                          ),
-                          Padding(
-                            padding:
-                                const EdgeInsets.only(top: 4.0, right: 13.0),
-                            child: Column(
-                              children: [
-                                const Text(
-                                  '21°',
-                                  style: TextStyle(
-                                    fontSize: 53,
-                                    color: Colors.white,
+                            Padding(
+                              padding:
+                                  const EdgeInsets.only(top: 4.0, right: 13.0),
+                              child: Column(
+                                children: [
+                                  const Text(
+                                    '21°',
+                                    style: TextStyle(
+                                      fontSize: 53,
+                                      color: Colors.white,
+                                    ),
                                   ),
-                                ),
-                                const SizedBox(
-                                  height: 19,
-                                ),
-                                Row(
-                                  children: const [
-                                    Text(
-                                      'H:29°',
-                                      style: TextStyle(
-                                          fontSize: 15, color: Colors.white),
-                                    ),
-                                    SizedBox(
-                                      width: 8,
-                                    ),
-                                    Text(
-                                      'L:15°',
-                                      style: TextStyle(
-                                          fontSize: 15, color: Colors.white),
-                                    ),
-                                  ],
-                                )
-                              ],
+                                  const SizedBox(
+                                    height: 19,
+                                  ),
+                                  Row(
+                                    children: const [
+                                      Text(
+                                        'H:29°',
+                                        style: TextStyle(
+                                            fontSize: 15, color: Colors.white),
+                                      ),
+                                      SizedBox(
+                                        width: 8,
+                                      ),
+                                      Text(
+                                        'L:15°',
+                                        style: TextStyle(
+                                            fontSize: 15, color: Colors.white),
+                                      ),
+                                    ],
+                                  )
+                                ],
+                              ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
                     ),
                     const SizedBox(
