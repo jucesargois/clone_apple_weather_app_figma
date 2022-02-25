d83da91c8807335508e2f85909c9e830e45d3ff5
Julio <jucesargois@gmail.com>
2022-02-24 22:27:52 -0300

    Refatoatoração: criado e inserido novos widgets

--- lib/app/weather_location/views/weather_location_view.dart
+++ lib/app/weather_location/views/weather_location_view.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
 
-import 'widgets/day_forecast_widget.dart';
+import 'widgets/card_air_quality_widget.dart';
+import 'widgets/card_day_forecast_widget.dart';
+import 'widgets/card_feels_like_widget.dart';
+import 'widgets/card_humidity_widget.dart';
+import 'widgets/card_rainfall_widget.dart';
+import 'widgets/card_sunrise_widget.dart';
+import 'widgets/card_temperature_widget.dart';
+import 'widgets/card_uv_index_widget.dart';
+import 'widgets/card_wind_widget.dart';
 
 class WeatherLocationView extends StatelessWidget {
   const WeatherLocationView({Key? key}) : super(key: key);
@@ -229,427 +237,57 @@ class WeatherLocationView extends StatelessWidget {
               const SizedBox(
                 height: 8,
               ),
-             DayForecastWidget(),
+              const CardDayForecastWidget(),
               const SizedBox(
                 height: 10,
               ),
-              Container(
-                width: 370,
-                height: 200,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15),
-                  color: Colors.grey,
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(15.0),
-                  child: Column(
-                    children: [
-                      Row(
-                        children: [
-                          Image.asset(
-                            'assets/images/icon_airquality_weather_location.png',
-                          ),
-                          const SizedBox(width: 5),
-                          const Text(
-                            'AIR QUALITY',
-                            style: TextStyle(fontSize: 15),
-                          ),
-                        ],
-                      ),
-                      const SizedBox(
-                        height: 6,
-                      ),
-                      const Align(
-                        alignment: Alignment.topLeft,
-                        child: Text(
-                          '56 - Moderate',
-                          style: TextStyle(fontSize: 23),
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 8,
-                      ),
-                      const Align(
-                        alignment: Alignment.topLeft,
-                        child: Text(
-                          'Air quality is 56, which is the same as\nyesterday at about this time.',
-                          style: TextStyle(fontSize: 17),
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      Image.asset(
-                          'assets/images/image_group_weather_location.png'),
-                      const SizedBox(
-                        height: 26,
-                      ),
-                      const Divider(
-                        height: 2,
-                        color: Colors.red,
-                      ),
-                      const SizedBox(
-                        height: 8,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          const Text(
-                            'See More',
-                            style: TextStyle(fontSize: 17),
-                          ),
-                          Image.asset(
-                              'assets/images/icon_arrow_weather_location.png')
-                        ],
-                      )
-                    ],
-                  ),
-                ),
-              ),
+              const CardAirQualityWidget(),
               const SizedBox(
                 height: 10,
               ),
-              Container(
-                width: 370,
-                height: 277,
-                decoration: BoxDecoration(
-                    color: Colors.grey,
-                    borderRadius: BorderRadius.circular(15)),
-                child: Padding(
-                  padding: const EdgeInsets.all(15.0),
-                  child: Column(
-                    children: [
-                      Row(
-                        children: [
-                          Image.asset(
-                            'assets/images/icon_temperature_weather_location.png',
-                          ),
-                          const SizedBox(width: 5),
-                          const Text(
-                            'TEMPERATURE',
-                            style: TextStyle(fontSize: 15),
-                          ),
-                        ],
-                      ),
-                      const SizedBox(
-                        height: 5,
-                      ),
-                      Image.asset(
-                        'assets/images/image_temperature_weather_location.png',
-                      ),
-                      const SizedBox(
-                        height: 5,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          const Text(
-                            'See More',
-                            style: TextStyle(fontSize: 17),
-                          ),
-                          Image.asset(
-                            'assets/images/icon_arrow_weather_location.png',
-                          )
-                        ],
-                      )
-                    ],
-                  ),
-                ),
-              ),
+              const CardTemperatureWidget(),
               const SizedBox(
                 height: 10,
               ),
               Padding(
-                padding: const EdgeInsets.only(top: 5, left: 20),
+                padding: const EdgeInsets.only(
+                  top: 5,
+                  left: 20,
+                ),
                 child: Row(
-                  children: [
-                    Container(
-                      width: 180,
-                      height: 165,
-                      decoration: BoxDecoration(
-                        color: Colors.grey,
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(15.0),
-                        child: Column(
-                          children: [
-                            Row(
-                              children: [
-                                Image.asset(
-                                  'assets/images/icon_sun_weather_location.png',
-                                ),
-                                const SizedBox(width: 5),
-                                const Text(
-                                  'UV INDEX',
-                                  style: TextStyle(fontSize: 15),
-                                ),
-                              ],
-                            ),
-                            const Align(
-                              alignment: Alignment.topLeft,
-                              child: Text(
-                                '0',
-                                style: TextStyle(fontSize: 36),
-                              ),
-                            ),
-                            const Align(
-                              alignment: Alignment.topLeft,
-                              child: Text(
-                                'Low',
-                                style: TextStyle(fontSize: 22),
-                              ),
-                            ),
-                            const SizedBox(
-                              height: 8,
-                            ),
-                            Image.asset(
-                                'assets/images/line_uv_index_weather_location.png'),
-                            const SizedBox(
-                              height: 3,
-                            ),
-                            const Align(
-                                alignment: Alignment.topLeft,
-                                child: Text('Low for the rest\nof the day.')),
-                          ],
-                        ),
-                      ),
-                    ),
-                    const SizedBox(
+                  children: const [
+                    CardUvIndexWidget(),
+                    SizedBox(
                       width: 11,
                     ),
-                    Container(
-                      width: 180,
-                      height: 165,
-                      decoration: BoxDecoration(
-                        color: Colors.grey,
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(15.0),
-                        child: Column(
-                          children: [
-                            Row(
-                              children: [
-                                Image.asset(
-                                  'assets/images/icon_sunrise_weather_location.png',
-                                ),
-                                const SizedBox(width: 5),
-                                const Text(
-                                  'SUNRISE',
-                                  style: TextStyle(fontSize: 15),
-                                ),
-                              ],
-                            ),
-                            const SizedBox(
-                              height: 6,
-                            ),
-                            const Align(
-                              alignment: Alignment.topLeft,
-                              child: Text(
-                                '6:28AM',
-                                style: TextStyle(fontSize: 35),
-                              ),
-                            ),
-                            const SizedBox(
-                              height: 8,
-                            ),
-                            Image.asset(
-                                'assets/images/icon_vector_weather_location.png'),
-                            const SizedBox(
-                              height: 6,
-                            ),
-                            const Align(
-                                alignment: Alignment.topLeft,
-                                child: Text('Sunset: 6:10PM',
-                                    style: TextStyle(fontSize: 18))),
-                          ],
-                        ),
-                      ),
+                    CardSunriseWidget(),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  top: 5,
+                  left: 20,
+                ),
+                child: Row(
+                  children: const [
+                    CardWindWidget(),
+                    SizedBox(
+                      width: 11,
                     ),
+                    CardRainfallWidget(),
                   ],
                 ),
               ),
               Padding(
                 padding: const EdgeInsets.only(top: 5, left: 20),
                 child: Row(
-                  children: [
-                    Container(
-                      width: 180,
-                      height: 165,
-                      decoration: BoxDecoration(
-                        color: Colors.grey,
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(15.0),
-                        child: Column(
-                          children: [
-                            Row(
-                              children: [
-                                Image.asset(
-                                  'assets/images/icon_wind_weather_location.png',
-                                ),
-                                const SizedBox(width: 5),
-                                const Text(
-                                  'WIND',
-                                  style: TextStyle(fontSize: 15),
-                                ),
-                              ],
-                            ),
-                            Image.asset(
-                                'assets/images/image_compass_weather_location.png')
-                          ],
-                        ),
-                      ),
-                    ),
-                    const SizedBox(
+                  children:const [
+                     CardFeelsLikeWidget(),
+                     SizedBox(
                       width: 11,
                     ),
-                    Container(
-                      width: 180,
-                      height: 165,
-                      decoration: BoxDecoration(
-                        color: Colors.grey,
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(15.0),
-                        child: Column(
-                          children: [
-                            Row(
-                              children: [
-                                Image.asset(
-                                  'assets/images/icon_rainfall_weather_location.png',
-                                ),
-                                const SizedBox(width: 5),
-                                const Text(
-                                  'RAINFALL',
-                                  style: TextStyle(fontSize: 15),
-                                ),
-                              ],
-                            ),
-                            const Align(
-                              alignment: Alignment.topLeft,
-                              child: Text(
-                                '0 mm',
-                                style: TextStyle(fontSize: 35),
-                              ),
-                            ),
-                            const Align(
-                              alignment: Alignment.topLeft,
-                              child: Text(
-                                'in last 24h',
-                                style: TextStyle(fontSize: 23),
-                              ),
-                            ),
-                            const SizedBox(
-                              height: 2,
-                            ),
-                            const Align(
-                                alignment: Alignment.topLeft,
-                                child: Text('4 mm expected in next 24h.',
-                                    style: TextStyle(fontSize: 17))),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 5, left: 20),
-                child: Row(
-                  children: [
-                    Container(
-                      width: 180,
-                      height: 165,
-                      decoration: BoxDecoration(
-                        color: Colors.grey,
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(15.0),
-                        child: Column(
-                          children: [
-                            Row(
-                              children: [
-                                Image.asset(
-                                  'assets/images/icon_sun_weather_location.png',
-                                ),
-                                const SizedBox(width: 5),
-                                const Text(
-                                  'FEELS LIKE',
-                                  style: TextStyle(fontSize: 15),
-                                ),
-                              ],
-                            ),
-                            const SizedBox(height: 5),
-                            const Align(
-                              alignment: Alignment.topLeft,
-                              child: Text(
-                                '73%',
-                                style: TextStyle(fontSize: 35),
-                              ),
-                            ),
-                            const SizedBox(height: 8),
-                            const Align(
-                              alignment: Alignment.topLeft,
-                              child: Text(
-                                'Similar to the\nactual\ntemperature',
-                                style: TextStyle(fontSize: 17),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    const SizedBox(
-                      width: 11,
-                    ),
-                    Container(
-                      width: 180,
-                      height: 165,
-                      decoration: BoxDecoration(
-                        color: Colors.grey,
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(15.0),
-                        child: Column(
-                          children: [
-                            Row(
-                              children: [
-                                Image.asset(
-                                  'assets/images/icon_humidity_weather_location.png',
-                                ),
-                                const SizedBox(width: 5),
-                                const Text(
-                                  'HUMIDITY',
-                                  style: TextStyle(fontSize: 15),
-                                ),
-                              ],
-                            ),
-                            const SizedBox(height: 5),
-                            const Align(
-                              alignment: Alignment.topLeft,
-                              child: Text(
-                                '73%',
-                                style: TextStyle(fontSize: 35),
-                              ),
-                            ),
-                            const SizedBox(height: 20),
-                            const Align(
-                              alignment: Alignment.topLeft,
-                              child: Text(
-                                'The dew point is 16° right now.',
-                                style: TextStyle(fontSize: 17),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
+                   CardHumidityWidget(),
                   ],
                 ),
               )
