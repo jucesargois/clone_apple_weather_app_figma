07ea7deb690a092dbeacae3d9954db5c12905efb
Julio <jucesargois@gmail.com>
2022-02-25 15:27:59 -0300

    Inserido widget SearchBarWidget e os widgets de CardLocationWidget

--- lib/app/home/views/home_view.dart
+++ lib/app/home/views/home_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'widgets/card_location_widget.dart';
+import 'widgets/search_bar_widget.dart';
+import 'package:intl/intl.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({Key? key}) : super(key: key);
@@ -35,233 +38,34 @@ class HomeView extends StatelessWidget {
               const SizedBox(
                 height: 8,
               ),
-              Container(
-                width: 370,
-                height: 40,
-                decoration: BoxDecoration(
-                  color: Colors.grey[900],
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Row(
-                  children: const [
-                    SizedBox(
-                      width: 8,
-                    ),
-                    Icon(
-                      Icons.search,
-                      color: Colors.grey,
-                    ),
-                    SizedBox(
-                      width: 4,
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(top: 19.0),
-                      child: SizedBox(
-                        width: 300,
-                        height: 35,
-                        child: TextField(
-                          decoration: InputDecoration(
-                            focusedBorder: InputBorder.none,
-                            enabledBorder: InputBorder.none,
-                            hintText: 'Search for a city or airport',
-                            hintStyle:
-                                TextStyle(fontSize: 19, color: Colors.grey),
-                          ),
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-              ),
+              const SearchBarWidget(),
               Padding(
                 padding: const EdgeInsets.only(
                   top: 15,
                 ),
                 child: Column(
                   children: [
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.pushReplacementNamed(
-                            context, "/weather_location");
-                      },
-                      child: Container(
-                        width: 400,
-                        height: 117,
-                        decoration: BoxDecoration(
-                          image: const DecorationImage(
-                              image: AssetImage(
-                                'assets/images/image_card_home_star.png',
-                              ),
-                              fit: BoxFit.fill),
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.only(
-                                top: 10.0,
-                                left: 13.0,
-                                bottom: 10,
-                              ),
-                              child: Column(
-                                children: const [
-                                  Text(
-                                    'My Location',
-                                    style: TextStyle(
-                                        fontSize: 25,
-                                        color: Colors.white,
-                                        fontWeight: FontWeight.bold),
-                                  ),
-                                  Text(
-                                    'Seongnam-si',
-                                    style: TextStyle(
-                                      fontSize: 16,
-                                      color: Colors.white,
-                                    ),
-                                  ),
-                                  SizedBox(
-                                    height: 28,
-                                  ),
-                                  Text(
-                                    'Partly Cloudy',
-                                    style: TextStyle(
-                                      fontSize: 16,
-                                      color: Colors.white,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                            Padding(
-                              padding:
-                                  const EdgeInsets.only(top: 4.0, right: 13.0),
-                              child: Column(
-                                children: [
-                                  const Text(
-                                    '21°',
-                                    style: TextStyle(
-                                      fontSize: 53,
-                                      color: Colors.white,
-                                    ),
-                                  ),
-                                  const SizedBox(
-                                    height: 19,
-                                  ),
-                                  Row(
-                                    children: const [
-                                      Text(
-                                        'H:29°',
-                                        style: TextStyle(
-                                            fontSize: 15, color: Colors.white),
-                                      ),
-                                      SizedBox(
-                                        width: 8,
-                                      ),
-                                      Text(
-                                        'L:15°',
-                                        style: TextStyle(
-                                            fontSize: 15, color: Colors.white),
-                                      ),
-                                    ],
-                                  )
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
+                    CardLocationWidget(
+                        'Boituva',
+                        DateFormat('hh:mm').format(DateTime.now()),
+                        'Partly Cloudy',
+                        '31°',
+                        'assets/images/image_card_home_star.png'),
                     const SizedBox(
                       height: 11,
                     ),
-                    Container(
-                      width: 400,
-                      height: 117,
-                      decoration: BoxDecoration(
-                        image: const DecorationImage(
-                            image: AssetImage(
-                              'assets/images/image_home_card_2.png',
-                            ),
-                            fit: BoxFit.fill),
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.only(
-                              top: 10.0,
-                              left: 13.0,
-                              bottom: 10,
-                            ),
-                            child: Column(
-                              children: const [
-                                Text(
-                                  'My Location',
-                                  style: TextStyle(
-                                      fontSize: 25,
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                                Text(
-                                  'Seongnam-si',
-                                  style: TextStyle(
-                                    fontSize: 16,
-                                    color: Colors.white,
-                                  ),
-                                ),
-                                SizedBox(
-                                  height: 28,
-                                ),
-                                Text(
-                                  'Partly Cloudy',
-                                  style: TextStyle(
-                                    fontSize: 16,
-                                    color: Colors.white,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                          Padding(
-                            padding:
-                                const EdgeInsets.only(top: 4.0, right: 13.0),
-                            child: Column(
-                              children: [
-                                const Text(
-                                  '21°',
-                                  style: TextStyle(
-                                    fontSize: 53,
-                                    color: Colors.white,
-                                  ),
-                                ),
-                                const SizedBox(
-                                  height: 19,
-                                ),
-                                Row(
-                                  children: const [
-                                    Text(
-                                      'H:29°',
-                                      style: TextStyle(
-                                          fontSize: 15, color: Colors.white),
-                                    ),
-                                    SizedBox(
-                                      width: 8,
-                                    ),
-                                    Text(
-                                      'L:15°',
-                                      style: TextStyle(
-                                          fontSize: 15, color: Colors.white),
-                                    ),
-                                  ],
-                                )
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
+                    CardLocationWidget(
+                        'Osasco',
+                        DateFormat('hh:mm').format(DateTime.now()),
+                        'Sorm',
+                        '15°',
+                        'assets/images/image_card_home_sky.png'),
+                    CardLocationWidget(
+                        'Manaus',
+                        DateFormat('hh:mm').format(DateTime.now()),
+                        'Heavy rain',
+                        '19°',
+                        'assets/images/image_card_home_star.png'),
                     const SizedBox(
                       height: 14,
                     ),
